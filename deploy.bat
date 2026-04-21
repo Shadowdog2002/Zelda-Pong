622f940cd264874709374a348a2dd544f75384e6
Shadowdog2002 <vijaykoduri@gmail.com>
2026-04-21 02:12:10 -0400

    remove git commands

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,4 @@
 @echo off
 pygbag --disable-sound-format-error --template autorun.tmpl .
 powershell -Command "Copy-Item -Path 'build/web/*' -Destination 'docs' -Recurse -Force"
-git add docs
-git commit -m "Update build %DATE% %TIME%"
-git push
+
